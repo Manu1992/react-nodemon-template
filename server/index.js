874072a24d4117b4a9cd49d800abaeaeb4ccda26
Manu1992 <manuel.maute92@gmail.com>
2016-11-29 11:11:06 +0100

    initial commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import express from 'express'
 let app = express()
 
 app.get('/*', (req, res) => {
-    res.send('hello!! world')
+    res.send('hello world!')
 })
 
 app.listen(8080, () => console.log(`Running on localhost:8080`))
